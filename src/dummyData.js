87c3813044d20c2a7e885d7decef4833bc6901b4
mikachou <chou1005.tw@gmail.com>
2023-09-21 16:50:18 +0800

    add logo and dummyData

---
+++ src/dummyData.js
@@ -0,0 +1,98 @@
+let matsuriData = [
+  {
+    id: "TK-SP-1",
+    season: "春天",
+    place: "東京",
+    eventName: "上野櫻花祭",
+    eventNameJP: "うえの桜まつり",
+    period: "3月中旬至3月下旬",
+    location: "上野恩賜公園",
+    feature: ["賞花", "賞夜櫻"],
+    transpotatin: [
+      {
+        line: "JR山手線",
+        station: "上野站",
+        route: "徒步3分鐘",
+      },
+    ],
+    webLink: "https://ueno.or.jp/",
+    desctioption:
+      "俗稱上野公園，以美麗的櫻花大道聞名的「上野恩賜公園」，是日本人最孰悉的賞櫻勝地。公園內有以染井吉野櫻、山櫻等不同種類，合計高達1000棵的櫻花一同齊放形成非常壯觀的美景。場內也有許多攤販，可以在公園內邊野餐邊賞櫻，加上夜晚也會點燈讓賞櫻氣氛更加熱鬧!",
+  },
+  {
+    id: "TK-SM-1",
+    season: "夏天",
+    place: "東京",
+    eventName: "隅田川煙火大會",
+    eventNameJP: "隅田川花火大会",
+    period: "7月下旬",
+    location: "淺草隅田川",
+    feature: ["夏日祭典", "煙火大會"],
+    transpotatin: [
+      {
+        line: "JR山手線、地下鐵銀座線、東武鐵道伊勢崎線、都營地下鐵淺草線",
+        station: "淺草站",
+        route: "徒步3~5分鐘",
+      },
+    ],
+    webLink: "https://www.sumidagawa-hanabi.com/",
+    desctioption:
+      "隅田川煙火大會可回溯至1733年，當時為祈求飢荒結束並撫慰因疫病犧牲的人們而開始的活動，被視為有史以來最早的煙火祭典。大會會有日本國內最大規模20000發的煙火綻放，是個非常受歡迎的活動。",
+  },
+  {
+    id: "TK-AT-1",
+    season: "秋天",
+    place: "東京",
+    eventName: "神宮外苑賞銀杏",
+    eventNameJP: "神宮外苑いちょうまつり",
+    period: "11月中旬至12月上旬",
+    location: "神宮外苑",
+    feature: ["賞花"],
+    transpotatin: [
+      {
+        line: "JR中央線",
+        station: "信濃町站",
+        route: "徒步10分鐘",
+      },
+      {
+        line: "地下鐵銀座線",
+        station: "青山一丁目站 | 外苑前站 ",
+        route: "徒步5分鐘",
+      },
+    ],
+    webLink: "http://www.meijijingugaien.jp/",
+    desctioption:
+      "位於東京市內的明治神宮外苑，由146棵銀杏樹所形成的300公尺的金色隧道而聞名的銀杏樹大道，每年在銀杏樹最美時舉辦的祭典，現場會有各式美食和精選商品的攤販以及親子活動，每年都吸引眾多遊客前來參加。",
+  },
+  {
+    id: "TK-WN-1",
+    season: "冬天",
+    place: "東京",
+    eventName: "淺草鷲神社例大祭 酉之市",
+    eventNameJP: "浅草鷲神社例大祭 酉の市",
+    period: "11月上旬至中旬",
+    location: "淺草鷲神社",
+    feature: ["神社祭典"],
+    transpotatin: [
+      {
+        line: "地下鐵日比谷線",
+        station: "入谷站(北口3號出口)",
+        route: "徒步7分鐘",
+      },
+      {
+        line: "TSUKUBA EXPRESS線",
+        station: "淺草站",
+        route: "徒步8分鐘",
+      },
+      {
+        line: "地下鐵淺草線/銀座線",
+        station: "淺草站",
+        route: "徒步15分鐘",
+      },
+    ],
+    webLink: "https://otorisama.or.jp/",
+    desctioption:
+      "每年11月的午夜12點，隨著一番太鼓敲響，代表祭典的開始。現場會有販賣著代表開運與商業繁榮的熊手御守的熊手店和眾多攤販，形成非常熱鬧的下町年末風景。",
+  },
+];
+export default matsuriData;
