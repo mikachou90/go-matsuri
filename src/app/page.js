d6297643ee31c2eb3e7d7b084e9843627eb193d3
mikachou <chou1005.tw@gmail.com>
2023-10-11 16:25:46 +0800

    create navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,16 @@
 import Image from "next/image";
+import Link from "next/link";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <header>
-      <Image src="/logo-large.png" width={200} height={20} alt="logo" />
+    <header className="pr-[10rem] pl-[10rem] fixed w-full">
+      <div className="flex justify-center mb-[0.5rem]">
+        <Link href="/">
+          <Image src="/logo-large.png" width="200" height="20" alt="logo" />
+        </Link>
+      </div>
+      <Navbar />
     </header>
   );
 }
