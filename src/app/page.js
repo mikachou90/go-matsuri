a8e01f6286de0a22bb5c8754b831730024976447
mikachou <chou1005.tw@gmail.com>
2023-10-11 09:35:16 +0800

    add logo on main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,9 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <header></header>;
+  return (
+    <header>
+      <Image src="/logo-large.png" width={200} height={20} alt="logo" />
+    </header>
+  );
 }
