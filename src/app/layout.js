9c20360cd01dd70a09e8f0ca2d3c1c203c02a1a8
mikachou <chou1005.tw@gmail.com>
2023-10-12 12:10:37 +0800

    adjust layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,9 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
+// 字型
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -10,8 +13,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="ch-TW">
+      <body className={inter.className}>
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
