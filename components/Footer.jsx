73ac548b25b904c7d5175b48e958710d3c1f672d
mikachou <chou1005.tw@gmail.com>
2023-10-20 09:09:11 +0800

    add content

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Footer() {
   return (
     <>
-      <div className="w-full">
+      <div className="w-full min-w-full">
         <div className="bg-gray-300 flex flex-col justify-center items-center py-8">
           <Image src="/logo-large-03.png" width="150" height="30" alt="logo" />
           <p className="text-zinc-400 mt-5">COPYRIGHT BY M.C</p>
