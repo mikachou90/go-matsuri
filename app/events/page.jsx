d7dd61775f663ff257aa29cf38d1a773d6dfef84
mikachou <chou1005.tw@gmail.com>
2023-10-25 16:50:39 +0800

    adjust

--- app/events/page.jsx
+++ app/events/page.jsx
@@ -1,6 +1,6 @@
 import EventList from "@/components/EventList";
 import Link from "next/link";
-import { getEvents } from "../../api/eventData";
+import { getEvents } from "@/api/eventData";
 
 const Events = async () => {
   const eventData = await getEvents();
@@ -11,6 +11,7 @@ const Events = async () => {
     <div className="py-10 px-20 mt-20">
       <div className="mb-10">
         <h1 className="text-3xl font-bold">祭典一覽</h1>
+
         <div
           className="mt-5 flex
         flex-col "
@@ -33,18 +34,20 @@ const Events = async () => {
       </div>
       <div>
         <div className=" mb-10 grid grid-cols-1 gap-8 justify-items-center md:grid-cols-2 md:gap-10 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 2xl:gap-12">
-          {eventData.map((data) => {
-            <Link href="/events/`${data.id}`">
-              <EventList
-                id={data.id}
-                picture={data.picture}
-                title={data.name}
-                period={data.period}
-                location={data.location}
-                feature={data.feature}
-              />
-            </Link>;
-          })}
+          {eventData
+            ? eventData.map((data) => {
+                <Link key={data.id} href={`/events/${data.id}`}>
+                  <EventList
+                    id={data.id}
+                    picture={data.picture}
+                    title={data.name}
+                    period={data.period}
+                    location={data.location}
+                    feature={data.feature}
+                  />
+                </Link>;
+              })
+            : "Loading..."}
         </div>
       </div>
     </div>
