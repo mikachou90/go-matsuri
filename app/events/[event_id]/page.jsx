951653fe7c9b525257dc8ec101ac5beb436a4ede
mikachou <chou1005.tw@gmail.com>
2023-10-27 16:12:55 +0800

    adjust

--- app/events/[event_id]/page.jsx
+++ app/events/[event_id]/page.jsx
@@ -5,14 +5,13 @@ import { getEventDetail } from "@/api/events/route";
 
 export function generateMetadata({ params }) {
   return {
-    title: `Go Matsuri! | ${params.event_id}`,
+    title: `Go Matsuri!`,
     description: "日本慶典情報",
   };
 }
 
 const Event = async ({ params }) => {
   const data = await getEventDetail(params.event_id);
-  console.log("[data in eventDetil]", data);
 
   return (
     <div className="mt-5">
@@ -21,13 +20,13 @@ const Event = async ({ params }) => {
         className="w-full flex flex-col items-center pt-[100px] pb-[50px] px-4"
       >
         <h1 className="font-bold text-2xl mb-5 lg:text-4xl ">{data.name}</h1>
-        <div className="px-5 xl:flex xl:mt-5">
+        <div className="px-5 flex flex-col items-center  xl:flex-row xl:mt-5">
           <Image
             src={data.picture || "/pic_missing.png"}
-            width={400}
+            width={300}
             height={300}
             alt="event picture"
-            className="md:w-[600px] lg:w-[800px]"
+            className="md:w-[500px] lg:w-[600px]"
           />
           <div className="w-[400px] mt-[30px] md:w-[600px] lg:w-[800px] xl:ml-10">
             <p className="font-bold text-lg md:text-xl lg:text-2xl  ">
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,9 +5,9 @@ export default function Footer() {
   return (
     <>
       <div className="w-full min-w-full">
-        <div className="bg-gray-300 flex flex-col justify-center items-center py-8">
-          <Image src="/logo-large-03.png" width="150" height="30" alt="logo" />
-          <p className="text-zinc-400 mt-5">COPYRIGHT BY M.C</p>
+        <div className="bg-gray-300 flex flex-col justify-center items-center py-4">
+          <Image src="/logo-large-03.png" width="120" height="30" alt="logo" />
+          <p className="text-zinc-400 mt-2">COPYRIGHT BY M.C</p>
         </div>
       </div>
     </>
