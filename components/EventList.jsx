2d82bfcaa1be64f7314f02997eaa56463d4489d1
mikachou <chou1005.tw@gmail.com>
2023-11-16 15:45:05 +0800

    adjust layout

--- components/EventList.jsx
+++ components/EventList.jsx
@@ -80,7 +80,7 @@ const EventList = () => {
   });
 
   return (
-    <div className="py-10 px-20 mt-20">
+    <>
       <div className="mb-10">
         <h1 className="text-3xl font-bold">祭典一覽</h1>
 
@@ -180,7 +180,7 @@ const EventList = () => {
           )}
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
