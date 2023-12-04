c01f8fc1b0b54f7216247b07f752beeabc6d6489
mikachou <chou1005.tw@gmail.com>
2023-12-04 14:58:58 +0800

    add favId to localStorage and render fav icon in page

--- components/EventList.jsx
+++ components/EventList.jsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { getDb } from "@/utils/firebaseInit";
 import { ref, onValue } from "firebase/database";
+import { GoHeart, GoHeartFill } from "react-icons/go";
 
 const EventList = () => {
   const router = useRouter();
@@ -17,13 +18,17 @@ const EventList = () => {
 
   // get all data from firebase
   const [eventsData, setEventsData] = useState({});
-  // make id = key and transfer data to array
-  const eventsArray = Object.keys(eventsData).map((eventKey) => {
-    return {
-      ...eventsData[eventKey],
-      id: eventKey,
-    };
-  });
+  const [newEventsArray, setNewEventsArray] = useState([]);
+
+  const renderFavBtn = () => {
+    let favIdArrs = JSON.parse(localStorage.getItem("isFavId")) || [];
+
+    setNewEventsArray((preArray) =>
+      preArray.map((event) =>
+        favIdArrs.includes(event.id) ? { ...event, isMyFav: true } : event
+      )
+    );
+  };
 
   useEffect(() => {
     const db = getDb();
@@ -33,7 +38,19 @@ const EventList = () => {
       (snapshot) => {
         const data = snapshot.val();
         setEventsData(data.events);
+
+        // make id = key and transfer data to array
+        const newArray = Object.keys(data.events).map((eventKey) => {
+          return {
+            ...data.events[eventKey],
+            id: eventKey,
+            isMyFav: false,
+          };
+        });
+        setNewEventsArray(newArray);
+        renderFavBtn();
       },
+
       {
         onlyOnce: true,
       }
@@ -41,12 +58,14 @@ const EventList = () => {
   }, []);
 
   // filter data
-  const filteredData = eventsArray?.filter((event) => {
-    const seasonMatch =
-      !currentParams.season || event.season === currentParams.season;
-    const cityMatch = !currentParams.city || event.city === currentParams.city;
-    return seasonMatch && cityMatch;
-  });
+  const filteredData =
+    newEventsArray?.filter((event) => {
+      const seasonMatch =
+        !currentParams.season || event.season === currentParams.season;
+      const cityMatch =
+        !currentParams.city || event.city === currentParams.city;
+      return seasonMatch && cityMatch;
+    }) ?? [];
 
   // render filter buttons
   let seasons = [];
@@ -63,12 +82,12 @@ const EventList = () => {
         (city, index, currentArray) => currentArray.indexOf(city) === index
       );
   } else {
-    seasons = eventsArray
+    seasons = filteredData
       .map(({ season }) => season)
       .filter(
         (season, index, currentArray) => currentArray.indexOf(season) === index
       );
-    cities = eventsArray
+    cities = filteredData
       .map(({ city }) => city)
       .filter(
         (city, index, currentArray) => currentArray.indexOf(city) === index
@@ -94,11 +113,33 @@ const EventList = () => {
     return router.push(`/events?${searchParams.toString()}`);
   };
 
+  // add to fav list button
+  const favBtnHandler = (e) => {
+    const id = e.currentTarget.dataset.id;
+
+    // 將喜歡的event id暫存進localStorage
+    let favIdArray = JSON.parse(localStorage.getItem("isFavId")) || [];
+    console.log({ favIdArray });
+
+    if (favIdArray.includes(id)) {
+      favIdArray = favIdArray.filter((favId) => favId !== id);
+    } else {
+      favIdArray.push(id);
+    }
+
+    let favIdStr = JSON.stringify(favIdArray);
+    localStorage.setItem("isFavId", favIdStr);
+    console.log({ favIdStr });
+    setNewEventsArray((preArray) =>
+      preArray.map((event) =>
+        event.id === id ? { ...event, isMyFav: !event.isMyFav } : event
+      )
+    );
+  };
+
   return (
     <>
       <div className="mb-10">
-        <h1 className="text-3xl font-bold">祭典一覽</h1>
-
         <div
           className="mt-5 flex
         flex-col "
@@ -153,38 +194,58 @@ const EventList = () => {
           {filteredData?.length > 0 ? (
             filteredData.map((event) => {
               return (
-                <li key={event.id} className="list-none">
-                  <Link href={`/events/${event.id}`}>
-                    <div
-                      key={event.id}
-                      className="bg-stone-200 w-[300px] h-[400px] rounded-lg flex flex-col justify-center items-center p-2 relative"
-                    >
+                <li key={event.id} id={event.id} className="list-none">
+                  <div
+                    key={event.id}
+                    id={event.id}
+                    className="bg-stone-200 w-[300px] h-[400px] rounded-lg flex flex-col justify-center items-center p-2 relative"
+                  >
+                    {event.isMyFav ? (
+                      <GoHeartFill
+                        className="cardIcon "
+                        size={25}
+                        color="red"
+                        onClick={favBtnHandler}
+                        data-id={event.id}
+                      />
+                    ) : (
+                      <GoHeart
+                        className="cardIcon "
+                        size={25}
+                        onClick={favBtnHandler}
+                        data-id={event.id}
+                      />
+                    )}
+
+                    <Link href={`/events/${event.id}`}>
                       <div className="tag flex rounded-b-lg items-center justify-center">
                         <p>{event.feature}</p>
                       </div>
-                      <Image
-                        src={event.picture || "/pic_missing.png"}
-                        width={200}
-                        height={80}
-                        alt="event picture"
-                      />
-                      <div className="mt-5 flex flex-col items-center">
-                        <p className="text-xl font-bold text-center">
-                          {event.name}
-                        </p>
+                      <div className="flex flex-col items-center">
+                        <Image
+                          src={event.picture || "/pic_missing.png"}
+                          width={200}
+                          height={80}
+                          alt="event picture"
+                        />
+                        <div className="mt-5 flex flex-col items-center">
+                          <p className="text-xl font-bold text-center">
+                            {event.name}
+                          </p>
 
-                        <p className="text-lg">{event.period}</p>
-                      </div>
-                      <div className="flex gap-2">
-                        <div className="text-sm p-2 rounded-lg flex bg-amber-500 text-white mt-2">
-                          <p className="">{event.season}</p>
+                          <p className="text-lg">{event.period}</p>
                         </div>
-                        <div className="text-sm p-2 rounded-lg flex bg-lime-500 text-white mt-2">
-                          <p>{event.city}</p>
+                        <div className="flex justify-center gap-2">
+                          <div className="text-sm p-2 rounded-lg flex bg-amber-500 text-white mt-2">
+                            <p className="">{event.season}</p>
+                          </div>
+                          <div className="text-sm p-2 rounded-lg flex bg-lime-500 text-white mt-2">
+                            <p>{event.city}</p>
+                          </div>
                         </div>
                       </div>
-                    </div>
-                  </Link>
+                    </Link>
+                  </div>
                 </li>
               );
             })
