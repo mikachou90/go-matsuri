c41c09bba1e75cfd467f09a93b8bf93ee3acb325
mikachou <chou1005.tw@gmail.com>
2024-04-01 17:17:59 +0800

    adjust header color and hover effect

--- components/Header.jsx
+++ components/Header.jsx
@@ -14,34 +14,34 @@ export default function Header() {
 
   return (
     <>
-      <header className="border-2 border-red-400 z-50 py-10 fixed top-0 p-3 w-full flex justify-between items-center bg-stone-200 lg:flex-col ">
+      <header className="z-10 py-10 fixed top-0 p-3 w-full flex justify-between items-center bg-amber-300 lg:flex-col ">
         <Link href="/">
           <Image
             src="/logo-large-03.png"
             width="500"
             height="20"
             alt="logo"
-            className="border-2 border-black ml-20 w-[20vw] lg:ml-0 lg:mb-10"
+            className="ml-20 w-[30vw] md:w-[20vw] lg:ml-0 lg:mb-10"
           />
         </Link>
 
-        <div className="border-2 border-yellow-600 flex justify-between items-center text-black font-bold text-lg lg:w-[50vw]">
+        <div className="flex justify-between items-center text-black font-bold text-lg lg:w-[50vw] lg:text-2xl">
           <ul className="hidden lg:flex justify-around items-center w-full">
-            <li className="hover:text-amber-400">
+            <li className="hover:text-white">
               <Link href="/">首頁</Link>
             </li>
-            <li className="hover:text-amber-400">
+            <li className="hover:text-white">
               <Link href="/events">祭典一覽</Link>
             </li>
 
-            <li className="hover:text-amber-400">
+            <li className="hover:text-white">
               <Link href="/postEvent">提供祭典</Link>
             </li>
-            <li className="hover:text-amber-400">
+            <li className="hover:text-white">
               <Link href="/myFavList">我的祭典</Link>
             </li>
 
-            <li className="hover:text-amber-400">
+            <li className="hover:text-white">
               <Link href="/about">關於本站</Link>
             </li>
           </ul>
@@ -58,33 +58,33 @@ export default function Header() {
           <div
             className={
               nav
-                ? "lg:none absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-stone-200 text-center ease-in duration-300"
-                : "lg:none absolute top-0 left-[100%] right-0 bottom-0 flex justify-center items-center w-full h-screen bg-stone-200 text-center ease-in duration-300"
+                ? "lg:none absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-amber-300  text-center ease-in duration-300"
+                : "lg:none absolute top-0 left-[100%] right-0 bottom-0 flex justify-center items-center w-full h-screen bg-amber-300    text-center ease-in duration-300"
             }
           >
             <ul>
-              <li className="text-2xl text-black mb-5 hover:text-amber-400">
+              <li className="text-2xl text-black mb-5 hover:text-white">
                 <Link href="/" onClick={handleMobileNav}>
                   首頁
                 </Link>
               </li>
-              <li className="text-2xl text-black mb-5 hover:text-amber-400">
+              <li className="text-2xl text-black mb-5 hover:text-white">
                 <Link href="/events" onClick={handleMobileNav}>
                   祭典一覽
                 </Link>
               </li>
 
-              <li className="text-2xl text-black mb-5 hover:text-amber-400">
+              <li className="text-2xl text-black mb-5 hover:text-white">
                 <Link href="/postEvent" onClick={handleMobileNav}>
                   提供祭典
                 </Link>
               </li>
-              <li className="text-2xl text-black mb-5 hover:text-amber-400">
+              <li className="text-2xl text-black mb-5 hover:text-white">
                 <Link href="/myFavList" onClick={handleMobileNav}>
                   我的祭典
                 </Link>
               </li>
-              <li className="text-2xl text-black mb-5 hover:text-amber-400">
+              <li className="text-2xl text-black mb-5 hover:text-white">
                 <Link href="/about" onClick={handleMobileNav}>
                   關於本站
                 </Link>
