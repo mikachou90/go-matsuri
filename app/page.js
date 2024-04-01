b3bba7891a2130706cb1606a541ea13f330a56e2
mikachou <chou1005.tw@gmail.com>
2024-04-01 17:18:33 +0800

    add about section

--- app/page.js
+++ app/page.js
@@ -1,49 +1,62 @@
+import Image from "next/image";
 import CurrentEvents from "../components/CurrentEvents";
 import IntroCards from "../components/IntroCards";
 import introText from "../app/introText";
 
 export default function Home() {
   return (
-    <>
-      <div>
-        <section className="w-full mt-10 hero-img ">
-          <div className="px-4 w-full h-full bg-black/60 flex flex-col justify-center  ">
-            <h1 className="font-bold text-white text-3xl text-left  md:text-5xl md:text-center">
-              Make your journey even better!
-            </h1>
-            <br />
-            <p className="font-bold text-white text-2xl text-left md:text-2xl md:text-center ">
-              找尋在你旅遊期間舉辦的祭典，讓旅程更添增美好回憶!
-            </p>
-          </div>
-        </section>
-        <section className="border-2 border-green-800 flex flex-col items-center my-10 p-5">
-          <div className="mb-5 ">
-            <h1 className="text-2xl my-6 font-bold md:text-3xl md:my-3">
-              搜尋、分享、儲存喜歡的祭典!
-            </h1>
-          </div>
-          <div className="p-5 md:flex md:justify-center md:items-center">
-            {introText.map((data) => {
-              return (
-                <IntroCards
-                  key={data.id}
-                  src={data.link}
-                  intro={data.intro}
-                  title={data.title}
-                  pageLink={data.pageLink}
-                />
-              );
-            })}
-          </div>
-        </section>
-        <section className="border-2 border-red-500 flex flex-col items-center my-10 p-5 ">
-          <h1 className="text-2xl font-bold my-6 md:text-3xl  md:my-3">
-            當季祭典
-          </h1>
-          <CurrentEvents />
-        </section>
-      </div>
-    </>
+    <div id="mainPage">
+      <section id="bannerSection" className=""></section>
+      <section id="greetSection" className="flex flex-col items-center p-10">
+        <h1
+          id="titleStyle"
+          className="mt-5 mb-10 text-2xl font-bold md:text-3xl"
+        >
+          About
+        </h1>
+        <p className="px-10 text-lg leading-4  sm:text-xl md:px-20 ">
+          「Go
+          Matsuri」是一個專為旅行愛好者打造的網站，提供搜尋和分享各種祭典資訊的平台。
+          <br />
+          無論您是正在計劃前往日本旅行，或是對當地文化感興趣，透過「Go
+          Matsuri」，您可以輕鬆搜索到各種祭典活動，從傳統的神社祭典到現代浪漫的燈飾活動，應有盡有。除了搜索功能外，我們還歡迎用戶分享自己的祭典體驗，讓更多人受益。這個平台不僅是一個資訊庫，更是一個共享和交流的社區。無論您是想獨自探索，還是與他人分享您的旅程，「Go
+          Matsuri」都是您不容錯過的旅行好幫手。立即加入我們，探索日本有趣的祭典世界！
+        </p>
+      </section>
+      <section
+        id="currentEventSection"
+        className="flex flex-col items-center p-10 bg-amber-100"
+      >
+        <h1
+          id="titleStyle"
+          className="mt-5 mb-10  text-2xl font-bold md:text-3xl "
+        >
+          當季祭典
+        </h1>
+        <CurrentEvents />
+      </section>
+      <section className="flex flex-col items-center p-10">
+        <h1
+          id="titleStyle"
+          className="mt-5 mb-10  text-2xl font-bold md:text-3xl "
+        >
+          搜尋、分享、儲存喜歡的祭典!
+        </h1>
+
+        <div className="sm:flex sm:justify-center sm:items-center">
+          {introText.map((data) => {
+            return (
+              <IntroCards
+                key={data.id}
+                src={data.link}
+                intro={data.intro}
+                title={data.title}
+                pageLink={data.pageLink}
+              />
+            );
+          })}
+        </div>
+      </section>
+    </div>
   );
 }
