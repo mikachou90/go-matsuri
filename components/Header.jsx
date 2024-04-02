c7d90860e7f795ea8b6c034f8c275ea9688f34e9
Mika Chou <128577628+mikachou90@users.noreply.github.com>
2024-04-02 16:50:36 +0800

    adjust header layout

--- components/Header.jsx
+++ components/Header.jsx
@@ -14,33 +14,31 @@ export default function Header() {
 
   return (
     <>
-      <header className="z-10 py-10 fixed top-0 p-3 w-full flex justify-between items-center bg-amber-300 lg:flex-col ">
+      <header className="z-10 py-5 fixed top-0  w-full flex justify-between items-center bg-amber-500 ">
         <Link href="/">
           <Image
             src="/logo-large-03.png"
             width="500"
             height="20"
             alt="logo"
-            className="ml-20 w-[30vw] md:w-[20vw] lg:ml-0 lg:mb-10"
+            className="ml-10 w-[30vw] md:w-[15vw] "
           />
         </Link>
 
-        <div className="flex justify-between items-center text-black font-bold text-lg lg:w-[50vw] lg:text-2xl">
-          <ul className="hidden lg:flex justify-around items-center w-full">
+        <div className="flex justify-between items-center  font-bold text-md md:w-[60vw] lg:text-lg">
+          <ul className="hidden md:flex justify-around items-center w-full">
             <li className="hover:text-white">
               <Link href="/">首頁</Link>
             </li>
             <li className="hover:text-white">
               <Link href="/events">祭典一覽</Link>
             </li>
-
             <li className="hover:text-white">
               <Link href="/postEvent">提供祭典</Link>
             </li>
             <li className="hover:text-white">
               <Link href="/myFavList">我的祭典</Link>
             </li>
-
             <li className="hover:text-white">
               <Link href="/about">關於本站</Link>
             </li>
@@ -49,42 +47,42 @@ export default function Header() {
           {/* mobile button */}
           <div
             onClick={handleMobileNav}
-            className="block absolute right-20 align-middle lg:hidden z-10 "
+            className="block absolute right-10 align-middle md:hidden z-10 "
           >
-            {nav ? <GrClose size={25} /> : <GrMenu size={25} />}
+            {nav ? <GrClose size={20} /> : <GrMenu size={20} />}
           </div>
 
           {/* mobile menu */}
           <div
             className={
               nav
-                ? "lg:none absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-amber-300  text-center ease-in duration-300"
-                : "lg:none absolute top-0 left-[100%] right-0 bottom-0 flex justify-center items-center w-full h-screen bg-amber-300    text-center ease-in duration-300"
+                ? "lg:none absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-amber-500  text-center ease-in duration-300"
+                : "lg:none absolute top-0 left-[100%] right-0 bottom-0 flex justify-center items-center w-full h-screen bg-amber-500    text-center ease-in duration-300"
             }
           >
             <ul>
-              <li className="text-2xl text-black mb-5 hover:text-white">
+              <li className="text-xl  mb-5 hover:text-white">
                 <Link href="/" onClick={handleMobileNav}>
                   首頁
                 </Link>
               </li>
-              <li className="text-2xl text-black mb-5 hover:text-white">
+              <li className="text-xl  mb-5 hover:text-white">
                 <Link href="/events" onClick={handleMobileNav}>
                   祭典一覽
                 </Link>
               </li>
 
-              <li className="text-2xl text-black mb-5 hover:text-white">
+              <li className="text-xl mb-5 hover:text-white">
                 <Link href="/postEvent" onClick={handleMobileNav}>
                   提供祭典
                 </Link>
               </li>
-              <li className="text-2xl text-black mb-5 hover:text-white">
+              <li className="text-xl  mb-5 hover:text-white">
                 <Link href="/myFavList" onClick={handleMobileNav}>
                   我的祭典
                 </Link>
               </li>
-              <li className="text-2xl text-black mb-5 hover:text-white">
+              <li className="text-xl mb-5 hover:text-white">
                 <Link href="/about" onClick={handleMobileNav}>
                   關於本站
                 </Link>
