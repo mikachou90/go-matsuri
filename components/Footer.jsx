ef5eb32673add5c9cdd12d267a9cf18b91a1286e
Mika Chou <128577628+mikachou90@users.noreply.github.com>
2024-04-02 16:50:12 +0800

    changed footer bg color

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <>
       <div className="w-full min-w-full">
-        <div className="bg-gray-300 flex flex-col justify-center items-center py-4">
+        <div className="bg-stone-200 flex flex-col justify-center items-center py-4">
           <Image src="/logo-large-03.png" width="120" height="30" alt="logo" />
           <p className="text-zinc-400 mt-2">此網站僅為個人作品練習用。</p>
         </div>
